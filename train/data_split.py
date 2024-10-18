21e43c186f00257926bda3e01a4b760609e146e6
gqlao0420 <64816955+gqlao0420@users.noreply.github.com>
2024-10-18 22:12:30 +0800

    Update data_split.py

--- train/data_split.py
+++ train/data_split.py
@@ -3,15 +3,24 @@ import os
 import shutil
 import random
 
-
+# 这段Python代码定义了一个函数 remove_files_in_dir，它接受一个目录路径 dir_path 作为参数，并删除该目录下的所有文件和子目录。
+# 使用此函数时要小心，因为它会永久删除文件和目录。在运行此函数之前，请确保你不想保留目录中的任何文件或目录
 def remove_files_in_dir(dir_path: str):
+    # 这行代码列出 dir_path 指定的目录下的所有文件和子目录，并迭代每一个项
     for f in os.listdir(dir_path):
+        # 对于每个项，使用 os.path.join 构建完整的文件路径
         file_path = os.path.join(dir_path, f)
+        # try...except 块：尝试执行删除操作，如果遇到任何异常，则捕获异常并打印错误信息
         try:
+            # 检查 file_path 是否是一个文件或符号链接
             if os.path.isfile(file_path) or os.path.islink(file_path):
+                # 如果 file_path 是文件或符号链接，使用 os.unlink 删除它
                 os.unlink(file_path)
+            # 如果 file_path 是一个目录，执行下面的 shutil.rmtree(file_path)
             elif os.path.isdir(file_path):
+                # 如果 file_path 是目录，使用 shutil.rmtree 删除该目录及其所有内容
                 shutil.rmtree(file_path)
+        # 如果删除操作失败，打印出未能删除的文件路径和失败原因
         except Exception as e:
             print("Failed to delete %s. Reason: %s" % (file_path, e))
 
